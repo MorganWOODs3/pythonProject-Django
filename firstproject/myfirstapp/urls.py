c0d4e44b33a1a8d86c405d3a8470f3ca15883731
MorganWOODs3 <92862519+MorganWOODs3@users.noreply.github.com>
2022-03-30 11:19:49 +0200

    dtgy

--- firstproject/myfirstapp/urls.py
+++ firstproject/myfirstapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns={
+urlpatterns = {
     path('index.html', views.index),
+    path('bonjour.html', views.bonjour),
 }
--- firstproject/myfirstapp/views.py
+++ firstproject/myfirstapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'myfirstapp/index.html')
+
+def bonjour(request):
+    nom = request.GET["nom"] # récupère la valeur du paramètre nom du formulaire
+    return render(request,'myfirstapp/bonjour.html',{"nom":nom}) # passe cette valeur à
+                                                                #la vue au travers du dictionnaire de contexte
+
